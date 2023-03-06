b827bd4b6e848a49baa0fa551282510d05ddedd8
Sayeem2004 <MohammadSSKC@gmail.com>
2023-03-05 20:16:58 -0500

    typing bug fix

--- lib/type/uInt16.ml
+++ lib/type/uInt16.ml
@@ -12,6 +12,6 @@ module UInt16 = struct
 
     let ui16_from_ui8 ui8 = from_int (UInt8.of_int ui8)
     let ui16_combine_ui8 (ui8_1: uint8) (ui8_2: uint8) : t =
-        UInt8.shift_left ui8_1 8 |> UInt8.logor ui8_2 |> from_int
+        UInt8.shift_left ui8_1 8 |> UInt8.logor ui8_2 |> ui16_from_ui8
 end
 type uint16 = UInt16.t
