7fe9e81c50cc14301c031e15a7c959c718ea0472
Sayeem2004 <MohammadSSKC@gmail.com>
2023-03-10 11:39:51 -0500

    Add more test functionality for uint16
    - Added tests for ui16_from_ui8 function
    - Added tests for ui16_combine_ui8 function

--- test/type/uInt16Test.ml
+++ test/type/uInt16Test.ml
@@ -1,4 +1,5 @@
 open OUnit2;;
+open Lib__UInt8;;
 open Lib__UInt16;;
 open UIntXTest;;
 
@@ -11,4 +12,29 @@ let (to_string_tests: test list) = [
     UInt16Test.to_string_test "To_String Over -> $0000" (UInt16.from_int UInt16Test.over) ("$0000");
 ];;
 
-let (tests: test list) = UInt16Test.tests @ to_string_tests;;
+let ui16_from_ui8_test (name: string) (a: uint8) (expected: uint16) =
+    name >:: fun _ ->
+        assert_equal expected (UInt16.ui16_from_ui8 a) ~printer:UInt16.to_string
+;;
+
+let (ui16_from_ui8_tests: test list) = [
+    ui16_from_ui8_test "UI16_From_UI8 Zero -> $0000" (UInt8.zero) (UInt16.zero);
+    ui16_from_ui8_test "UI16_From_UI8 Ones -> $0001" (UInt8.one) (UInt16.one);
+    ui16_from_ui8_test "UI16_From_UI8 Maxx -> $00FF" (UInt8.max_value) (UInt16.from_int 0xFF);
+];;
+
+let ui16_combine_ui8_test (name: string) (a: uint8) (b: uint8) (expected: uint16) =
+    name >:: fun _ ->
+        assert_equal expected (UInt16.ui16_combine_ui8 a b) ~printer:UInt16.to_string
+;;
+
+let (ui16_combine_ui8_tests: test list) = [
+    ui16_combine_ui8_test "UI16_Combine_UI8 Zero Zero -> $0000" (UInt8.zero) (UInt8.zero) (UInt16.zero);
+    ui16_combine_ui8_test "UI16_Combine_UI8 Zero Ones -> $0001" (UInt8.zero) (UInt8.one) (UInt16.one);
+    ui16_combine_ui8_test "UI16_Combine_UI8 Ones Zero -> $0100" (UInt8.one) (UInt8.zero) (UInt16.from_int 0x0100);
+    ui16_combine_ui8_test "UI16_Combine_UI8 Ones Ones -> $0101" (UInt8.one) (UInt8.one) (UInt16.from_int 0x0101);
+    ui16_combine_ui8_test "UI16_Combine_UI8 Maxx Maxx -> $FFFF" (UInt8.max_value) (UInt8.max_value) (UInt16.max_value);
+];;
+
+let (tests: test list) = UInt16Test.tests @ to_string_tests
+    @ ui16_from_ui8_tests @ ui16_combine_ui8_tests;;
