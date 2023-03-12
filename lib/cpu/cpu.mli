aaa53609c50c651b8ef77bfa57230ed5a2ccd51f
Evan Vera <evan.vera@hotmail.com>
2023-03-12 00:53:52 -0500

    Implemented ability to write to RAM

---
+++ lib/cpu/cpu.mli
@@ -0,0 +1,23 @@
+module CPU :
+  sig
+    type cpu_flags = {
+      carr_bit : bool;
+      zero : bool;
+      interrupt : bool;
+      decimal : bool;
+      negative : bool;
+      overflow : bool;
+      break : bool;
+      reserved : bool;
+    }
+    type t = {
+      accumulator : UInt8.uint8;
+      register_X : UInt8.uint8;
+      register_Y : UInt8.uint8;
+      program_counter : UInt16.uint16;
+      ram : Ram.RAM.t;
+      flags : cpu_flags;
+    }
+    val fetch_ui8 : t -> UInt16.uint16 -> UInt8.uint8
+    val fetch_ui16 : t -> UInt16.uint16 -> UInt16.uint16
+  end
