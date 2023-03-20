afdba3ae176407dfc629e8edc11add32345b74cc
Evan Vera <evan.vera@hotmail.com>
2023-03-19 21:40:37 -0400

    Added dev branch, changed cpu flag structure and instructions - Made [cpu.flags] a uint8, and added functions [flag_on] [flag_off]   and [flag_flip] to set these flags - Changed memory modes to add with carry

--- lib/cpu/cpu.ml
+++ lib/cpu/cpu.ml
@@ -3,16 +3,15 @@ open Lib__UInt16
 open Lib__Ram
 
 module CPU = struct
-    type cpu_flags = {
-        carr_bit: bool;
-        zero: bool;
-        interrupt: bool;
-        decimal: bool;
-        negative: bool;
-        overflow: bool;
-        break: bool;
-        reserved: bool;
-    };;
+    type flag =
+    | Carry
+    | Zero
+    | Interrupt
+    | Decimal
+    | Break
+    | Reserved
+    | Overflow
+    | Sign
 
     type t = {
         accumulator: uint8;
@@ -20,11 +19,47 @@ module CPU = struct
         register_Y: uint8;
         program_counter: uint16;
         ram: RAM.t;
-        flags: cpu_flags;
+        flags: uint8;
     };;
+
+    let carr_uint8 (cpu : t) : uint8 =
+        UInt8.logand cpu.flags (UInt8.from_int 0b00000001)
+
+    let flag_on (cpu : t) (fl : flag) =
+        match fl with
+        | Carry -> {cpu with flags = UInt8.logor cpu.flags (UInt8.from_int 0b00000001)}
+        | Zero -> {cpu with flags = UInt8.logor cpu.flags (UInt8.from_int 0b00000010)}
+        | Interrupt -> {cpu with flags = UInt8.logor cpu.flags (UInt8.from_int 0b00000100)}
+        | Decimal -> {cpu with flags = UInt8.logor cpu.flags (UInt8.from_int 0b00001000)}
+        | Break -> {cpu with flags = UInt8.logor cpu.flags (UInt8.from_int 0b00010000)}
+        | Reserved -> {cpu with flags = UInt8.logor cpu.flags (UInt8.from_int 0b00100000)}
+        | Overflow -> {cpu with flags = UInt8.logor cpu.flags (UInt8.from_int 0b01000000)}
+        | Sign -> {cpu with flags = UInt8.logor cpu.flags (UInt8.from_int 0b10000000)}
+
+    let flag_off (cpu : t) (fl : flag) =
+        match fl with
+        | Carry -> {cpu with flags = UInt8.logand cpu.flags (UInt8.from_int 0b11111110)}
+        | Zero -> {cpu with flags = UInt8.logand cpu.flags (UInt8.from_int 0b11111101)}
+        | Interrupt -> {cpu with flags = UInt8.logand cpu.flags (UInt8.from_int 0b11111011)}
+        | Decimal -> {cpu with flags = UInt8.logand cpu.flags (UInt8.from_int 0b11110111)}
+        | Break -> {cpu with flags = UInt8.logand cpu.flags (UInt8.from_int 0b11101111)}
+        | Reserved -> {cpu with flags = UInt8.logand cpu.flags (UInt8.from_int 0b11011111)}
+        | Overflow -> {cpu with flags = UInt8.logand cpu.flags (UInt8.from_int 0b10111111)}
+        | Sign -> {cpu with flags = UInt8.logand cpu.flags (UInt8.from_int 0b01111111)}
+
+    let flag_on (cpu : t) (fl : flag) =
+        match fl with
+        | Carry -> {cpu with flags = UInt8.logxor cpu.flags (UInt8.from_int 0b00000001)}
+        | Zero -> {cpu with flags = UInt8.logxor cpu.flags (UInt8.from_int 0b00000010)}
+        | Interrupt -> {cpu with flags = UInt8.logxor cpu.flags (UInt8.from_int 0b00000100)}
+        | Decimal -> {cpu with flags = UInt8.logxor cpu.flags (UInt8.from_int 0b00001000)}
+        | Break -> {cpu with flags = UInt8.logxor cpu.flags (UInt8.from_int 0b00010000)}
+        | Reserved -> {cpu with flags = UInt8.logxor cpu.flags (UInt8.from_int 0b00100000)}
+        | Overflow -> {cpu with flags = UInt8.logxor cpu.flags (UInt8.from_int 0b01000000)}
+        | Sign -> {cpu with flags = UInt8.logxor cpu.flags (UInt8.from_int 0b10000000)}
 
     let fetch_ui8 (cpu: t) (addr: uint16) : uint8 = RAM.read_ui8 cpu.ram addr;;
     let fetch_ui16 (cpu: t) (addr: uint16) : uint16 = RAM.read_ui16 cpu.ram addr;;
-    let write_ui8 (cpu : t) (addr : uint16) (value : uint8) = RAM.write_ui8 cpu.ram addr value;;
-    let write_ui16 (cpu : t) (addr : uint16) (value : uint16) = RAM.write_ui16 cpu.ram addr value;;
+    (* let write_ui8 (cpu : t) (addr : uint16) (value : uint8) = RAM.write_ui8 cpu.ram addr value;;
+    let write_ui16 (cpu : t) (addr : uint16) (value : uint16) = RAM.write_ui16 cpu.ram addr value;; *)
 end
--- lib/cpu/instructions.ml
+++ lib/cpu/instructions.ml
@@ -18,39 +18,64 @@ module Instructions = struct
   | ZeropageX
   | ZeropageY
 
-  let decode_operand (cpu : CPU.t) (mode : memory_mode) : uint16 =
+  type operand = {
+    u8 : uint8;
+    u16 : uint16;
+  }
+
+  let decode_operand_ui16 (cpu : CPU.t) (mode : memory_mode) : uint16 =
     match mode with
     | Accumulator -> UInt16.ui16_from_ui8 cpu.accumulator
+
     | Absolute -> CPU.fetch_ui16 cpu cpu.program_counter
-    | AbsoluteX -> CPU.fetch_ui16 cpu cpu.program_counter 
-    |> UInt16.add (UInt16.ui16_from_ui8 cpu.register_X)
-    | AbsoluteY -> CPU.fetch_ui16 cpu cpu.program_counter 
-    |> UInt16.add (UInt16.ui16_from_ui8 cpu.register_Y)
+
+    | AbsoluteX -> UInt16.add (CPU.fetch_ui16 cpu cpu.program_counter) 
+    (UInt16.ui16_from_ui8 cpu.register_X) |> UInt16.add (UInt16.ui16_from_ui8 CPU.carr_uint8 cpu.flags)
+
+    | AbsoluteY -> UInt16.add_carry (CPU.fetch_ui16 cpu cpu.program_counter) 
+    (UInt16.ui16_from_ui8 cpu.register_Y) cpu.flags.carr_bit
+
     | Immediate -> CPU.fetch_ui8 cpu cpu.program_counter |> UInt16.ui16_from_ui8
-    | Implied -> UInt16.zero
+
+    | Implied -> UInt16.zero (* TODO *)
+
     | Indirect -> CPU.fetch_ui16 cpu cpu.program_counter 
     |> CPU.fetch_ui16 cpu
-    | XIndirect -> CPU.fetch_ui8 cpu cpu.program_counter
-    |> UInt8.add cpu.register_X |> UInt16.ui16_from_ui8
-    | IndirectY -> CPU.fetch_ui8 cpu cpu.program_counter 
-    |> UInt8.add cpu.register_Y |> UInt16.ui16_from_ui8
+
+    | XIndirect -> CPU.fetch_ui16 cpu cpu.program_counter
+    |> UInt16.add (UInt16.ui16_from_ui8 cpu.register_X) |> CPU.fetch_ui8 cpu |> UInt16.ui16_from_ui8
+
+    | IndirectY -> UInt16.add_carry (CPU.fetch_ui16 cpu cpu.program_counter) 
+    (UInt16.ui16_from_ui8 cpu.register_Y) cpu.flags.carr_bit |> CPU.fetch_ui8 cpu |> UInt16.ui16_from_ui8
+
     | Relative -> CPU.fetch_ui8 cpu cpu.program_counter |> UInt16.ui16_from_ui8
     |> UInt16.add cpu.program_counter
-    | Zeropage -> CPU.fetch_ui8 cpu cpu.program_counter |> UInt16.ui16_from_ui8 
+
+    | Zeropage -> CPU.fetch_ui8 cpu cpu.program_counter |> UInt16.ui16_from_ui8
+
     | ZeropageX -> CPU.fetch_ui8 cpu cpu.program_counter 
     |> UInt8.add cpu.register_X |> UInt16.ui16_from_ui8
+    
     | ZeropageY -> CPU.fetch_ui8 cpu cpu.program_counter 
     |> UInt8.add cpu.register_Y |> UInt16.ui16_from_ui8
 
-    let adc_op (cpu : CPU.t) (mode : memory_mode) =
-      let add_op = decode_operand cpu mode in
-      {cpu with accumulator = UInt8.add cpu.accumulator (add_op |> UInt16.to_int |> UInt8.from_int) }
-
-    let and_op (cpu : CPU.t) (mode : memory_mode) = 
-      let add_op = decode_operand cpu mode in
-      {cpu with accumulator = UInt8.logand cpu.accumulator (add_op |> UInt16.to_int |> UInt8.from_int) }
+    let populate_operand_ui16 (ui16 : uint16) =
+        {
+            u8 = UInt16.to_int ui16 |> UInt8.from_int;
+            u16 = ui16;
+        }
     
-    let asl_op (cpu : CPU.t) (mode : memory_mode) =
-      if (mode = Accumulator) then {cpu with accumulator = UInt8.shift_left cpu.accumulator 1 }
-      else Ram.RAM.write_ui8
+    let decode_operand (cpu : CPU.t) (mode : memory_mode) =
+        decode_operand_ui16 cpu mode |> populate_operand_ui16
+
+    let adc_op (cpu : CPU.t) (mode : memory_mode) =
+        let operand = decode_operand cpu mode in
+        let summed_acc = UInt8.add_carry cpu.accumulator operand.u8 cpu.flags.carr_bit in
+        if (UInt8.compare summed_acc cpu.accumulator < 0) then {cpu with accumulator = summed_acc; flags = {cpu.flags with carr_bit = true}} else {cpu with accumulator = summed_acc}
+
+    let and_op (cpu : CPU.t) (mode : memory_mode) =
+      let operand = decode_operand cpu mode in
+      {cpu with accumulator = UInt8.logand operand.u8 cpu.accumulator}
+    
+    
 end
