ed0cb9babdfbc417d0526ec5465c1f7adce7c53d
Evan Vera <evan.vera@hotmail.com>
2023-03-22 19:55:03 -0400

    Edited infix for logxor and logor in [uint16] and [uint8] - [logxor] now [|\|] for [uint16] and [|\] for [uint8] - [logor] now [|||] for [uint16] and [||.] for [uint8]
    Added opcodes to instructions
    - [lda_op] loads memory into accumulator
    - [ldx_op] loads memory into register X
    - [ldy_op] loads memory into register Y
    - [lsr_op] shifts either accumulator or memory by 1 bit
    - [nop_ap] is no operation
    - [ora_op] performs logical or on accumulator with contents
      of address

--- lib/type/uInt8.mli
+++ lib/type/uInt8.mli
@@ -39,6 +39,9 @@ val ( %% ) : uint8 -> uint8 -> uint8
 val ( &&. ) : uint8 -> uint8 -> uint8
 (** Infix alias for UInt8.logand. *)
 
+val ( |/. ) : uint8 -> uint8 -> uint8
+(** Infix alias for UInt8.logxor. *)
+
 val ( ||. ) : uint8 -> uint8 -> uint8
 (** Infix alias for UInt8.logor. *)
 
@@ -59,6 +62,9 @@ val ( ?> ) : uint8 -> uint8 -> bool
 
 val ( ?- ) : uint8 -> bool
 (** Returns true if the most significant bit of a uint8 is [1]. *)
+
+val ( ?+ ) : uint8 -> bool
+(** Returns true if the least significant bit of a uint8 is [1]. *)
 
 val ( ?* ) : uint8 -> bool
 (** Returns true if the [uint8] is [0]. *)
